4ae26de1d0f2ef329f92cebe552e24b3dc4647d7
PeasantL <samuel.tan99@rocketmail.com>
2024-04-29 19:27:35 +0800

    Small fixed. Added inference parameters to readme.

--- README.md
+++ README.md
@@ -12,7 +12,16 @@ cd anime-segmentation
 ```
 
 
-## Inference
+## Inference Parameters
 
-`python inference.py --net isnet_is --ckpt path/to/isnet_is.ckpt --data-dir path/to/input_data --out out --img-size 1024 --only-matted`
+| Flag | Default | Desc |
+| --- | --- | --- |
+| --net |  isnet_is  | net name |
+| --ckpt | saved_models/isnet_is.ckpt | model checkpoint path |
+| --data | input | input data dir |
+| --out | out | output dir |
+| --img-size | 1024 | Hyperparameter, input image size of the net |
+| --device | cuda:0 | cpu or cuda:0 |
+| --fp32 | False | disable mix precision |
+| --only-matted | True | only output matted image |
 
--- run.sh
+++ run.sh
@@ -6,14 +6,12 @@ else
     echo "Virtual environment does not exist. Creating virtual environment."
     python3 -m venv .venv
     source "./.venv/bin/activate"
-    pip install -r requirements.txt  # Ensure to use -r to install from a requirements file
-fi
+    pip install -r requirements.txt  
 
 # Check if the model checkpoint file exists
-if [ ! -f "./saved_models/isnetis.ckpt" ]; then  # Fixed syntax error in if statement
+if [ ! -f "./saved_models/isnetis.ckpt" ]; then  
     echo "Downloading model checkpoint."
-    mkdir -p ./saved_models  # Ensure the directory exists
-    # Corrected curl command to specify the output file path with -o
+    mkdir -p ./saved_models  
     curl -L "https://huggingface.co/skytnt/anime-seg/resolve/main/isnetis.ckpt?download=true" -o "./saved_models/isnetis.ckpt"
 fi
 
