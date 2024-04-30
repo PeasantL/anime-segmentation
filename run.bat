fb92a73fdab3e95aa0a3e8acd88a53500c0f94f7
PeasantL <samuel.tan99@rocketmail.com>
2024-04-30 15:50:45 +0800

    Added bat script for windows.

---
+++ run.bat
@@ -0,0 +1,22 @@
+@echo off
+
+REM Check if the activate script exists
+IF EXIST ".venv\Scripts\activate.bat" (
+    echo Activating existing virtual environment.
+    CALL ".venv\Scripts\activate.bat"
+) ELSE (
+    echo Virtual environment does not exist. Creating virtual environment.
+    python -m venv .venv
+    CALL ".venv\Scripts\activate.bat"
+    pip install -r requirements.txt
+)
+
+REM Check if the model checkpoint file exists
+IF NOT EXIST "saved_models\isnetis.ckpt" (
+    echo Downloading model checkpoint.
+    mkdir saved_models
+    curl -L "https://huggingface.co/skytnt/anime-seg/resolve/main/isnetis.ckpt?download=true" -o "saved_models\isnetis.ckpt"
+)
+
+REM Run the inference script
+python inference.py
